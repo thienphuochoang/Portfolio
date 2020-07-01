724fa21ab3bfa52d2b911c5fffd8df4f07755129
bibubam <34785098+bibubam@users.noreply.github.com>
2020-07-01 22:55:48 +0700

    upload all files from htp_maya

---
+++ htp_maya/exporter/controller/controller.py
@@ -0,0 +1,178 @@
+import sys
+import os
+from functools import partial
+import importlib
+try:
+    from PySide2 import QtGui, QtCore, QtWidgets
+    from PySide2 import __version__
+    from PySide2.QtUiTools import QUiLoader
+    from shiboken2 import wrapInstance
+except ImportError:
+    from PySide import QtGui, QtCore
+    import PySide.QtGui as QtWidgets
+    from PySide import __version__
+    from PySide.QtUiTools import QUiLoader
+    from shiboken import wrapInstance
+
+
+try:
+    import maya.OpenMayaUI as omui
+    import maya.mel as mel
+    import maya.cmds as cmds
+    application = "Maya"
+except ImportError:
+    import MaxPlus 
+    import MaxPlusExtend
+    import pymxs
+    rt = pymxs.runtime
+    application = "Max"
+
+
+#import current_working_directory
+#reload(current_working_directory)
+#path_project, name_project = current_working_directory.get_cwd()
+
+
+mayaMainWindowPtr = omui.MQtUtil.mainWindow() 
+mayaMainWindow = wrapInstance(long(mayaMainWindowPtr), QtWidgets.QWidget)  # create a maya Main window
+
+
+def import_file(str_module):
+    """import a module from string"""
+    nameModule = importlib.import_module(str_module)
+    reload(nameModule)
+    return nameModule
+    
+#import Function____________________
+exporter_func = import_file(r"htp_maya.exporter.function.exporter_function")
+exporter_func.createTempFolder()
+exporter_mainUI = import_file(r"lib.ui.Exporter_MainUI")
+
+def import_add_id_ui():
+    pass
+
+
+class MainExporter(QtWidgets.QMainWindow, exporter_mainUI.Ui_MainWindowExporter):
+    def __init__(self):
+        super(MainExporter, self).__init__(parent=mayaMainWindow)
+        self.setupUi(self)
+        self.changeSelfRadioButtonText()
+        self.connectUI()
+
+
+    def connectUI(self):
+        self.btnExport.clicked.connect(
+            lambda: exporter_func.exportFile(
+                self.queryRadioButtonStatus(), 
+                self.queryCheckboxBinaryStatus(), 
+                self.queryCheckboxInstanceStatus()))
+        self.btnImport.clicked.connect(
+            lambda: exporter_func.importFile(
+                self.queryRadioButtonStatus(), 
+                self.queryCheckboxLockNormalStatus(), 
+                self.queryCheckboxGroup()))
+        self.btnAddID.clicked.connect(lambda: import_add_id_ui())
+
+        self.btnFixMaterial.clicked.connect(
+            partial(exporter_func.replaceMaterials))
+        self.btnExportWorking.clicked.connect(
+            lambda: exporter_func.exportWorkingFiles(
+                self.queryRadioButtonStatus(), 
+                self.queryCheckboxBinaryStatus(), 
+                self.queryCheckboxInstanceStatus()))
+        self.btnImportWorking.clicked.connect(
+            lambda: exporter_func.importWorkingFiles(
+                self.queryRadioButtonStatus(), 
+                self.queryCheckboxLockNormalStatus(), 
+                self.queryCheckboxGroup()))
+        self.btnExportHigh.clicked.connect(
+            partial(exporter_func.exportHighPoly))
+        self.btnExportLow.clicked.connect(
+            partial(exporter_func.exportLowPoly))
+        self.btnExportSeparatePart.clicked.connect(
+            lambda: exporter_func.exportSeparatePart(
+                self.queryRadioButtonStatus(), 
+                self.queryCheckboxBinaryStatus(), 
+                self.queryCheckboxInstanceStatus()))
+        self.btnOpenFolder.clicked.connect(
+            partial(exporter_func.openTempFolder))
+        self.btnDeleteFolder.clicked.connect(
+            partial(exporter_func.deleteTempFolderContents))
+        self.btnExportSP.clicked.connect(
+            partial(exporter_func.exportToSubstancePainter))
+
+        
+    def changeSelfRadioButtonText(self):
+        self.rbSelf.setText("MA")
+        #self.cbKeepID.setText("")
+        # self.cbKeepID.setUpdatesEnabled = True
+
+
+    def queryRadioButtonStatus(self):
+        rbFBX = self.rbFBX.isChecked()
+        rbObjStatus = self.rbObj.isChecked()
+        rbSelfStatus = self.rbSelf.isChecked()
+        if rbFBX:
+            return "FBX"
+        elif rbObjStatus:
+            return "OBJ"
+        elif rbSelfStatus:
+            return "Self"
+
+        
+    def queryKeepIDStatus(self):
+        cbKeepID = self.cbKeepID.isChecked()
+        if cbKeepID:
+            return "On"
+        else:
+            return "Off"
+
+        
+    def queryCheckboxBinaryStatus(self):
+        cbBinaryStatus = self.cbBinary.isChecked()
+        if cbBinaryStatus:
+            return "false"
+        else:
+            return "true"
+
+
+    def queryCheckboxInstanceStatus(self):
+        cbKeepInstanceStatus = self.cbKeepInstance.isChecked()
+        if cbKeepInstanceStatus:
+            return "true"
+        else:
+            return "false"
+
+
+    def queryCheckboxLockNormalStatus(self):
+        cbKeepLockVertexNormalStatus = self.cbKeepVertexNormal.isChecked()
+        if cbKeepLockVertexNormalStatus:
+            return "false"
+        else:
+            return "true"
+
+
+    def queryCheckboxGroup(self):
+        cbKeepGroup = self.cbKeepGroup.isChecked()
+        if cbKeepGroup:
+            return "true"
+        else:
+            return "false"
+
+def check_exist_window(name_window):
+    if application == "Maya":
+        if (cmds.window(name_window, exists=True)):
+            cmds.deleteUI(name_window, wnd=True)
+
+
+#check_exist_window("ExporterAddID")
+check_exist_window("MainWindowExporter")
+winExporter = MainExporter()
+winExporter.show()
+
+
+
+
+
+
+
---
+++ htp_maya/exporter/controller/main.py
@@ -0,0 +1,5 @@
+
+import controller
+reload(controller)
+
+print dir(controller)
