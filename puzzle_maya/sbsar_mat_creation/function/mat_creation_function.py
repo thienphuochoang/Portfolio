2a5e943095592685a217cfbee6ad890e705ed551
PhuocHoang <htphuoc1995@gmail.com>
2021-04-16 00:33:50 +0700

    update baking function

--- puzzle_maya/sbsar_mat_creation/function/mat_creation_function.py
+++ puzzle_maya/sbsar_mat_creation/function/mat_creation_function.py
@@ -64,55 +64,193 @@ class MatCreationFunction():
 		heightResolution = self.convertResolution(args[2])
 		resolution = widthResolution + "," + heightResolution
 
-		print (args)
-		if args[0][0] == True:
+		self.SBSBakerLocation = self.getSBSBaker()
+		if self.SBSBakerLocation != None:
 			self.exportFBXToBake(self.outputPath)
-			self.bakeAOFromSBSBaker(fbxPath,
-									outputName,
-									"ambient_occlusion",
-									self.outputPath,
-									resolution,
-									str(args[3]),
-									str(args[-1]).lower(),
-									str(int(args[4]) - 1),
-									args[5],
-									"true",
-									args[6])
+			if args[0][0] == True:
+				self.bakeAOFromSBSBaker(fbxPath,
+										outputName,
+										"ambient_occlusion",
+										self.outputPath,
+										resolution,
+										str(args[3]),
+										str(args[-1]).lower(),
+										str(int(args[4]) - 1),
+										args[5],
+										"false",
+										args[6])
+
+			if args[0][1] == True:
+				self.bakeCurvatureFromSBSBaker(fbxPath,
+										outputName,
+										"curvature",
+										self.outputPath,
+										resolution,
+										str(int(args[4]) - 1),
+										args[5])
+
+			if args[0][2] == True:
+				self.bakeWorldSpaceNormalFromSBSBaker(fbxPath,
+										outputName,
+										"world_space_normal",
+										self.outputPath,
+										resolution,
+										str(int(args[4]) - 1),
+										args[5],
+										args[6])
+
+			if args[0][3] == True:
+				self.bakePositionFromSBSBaker(fbxPath,
+										outputName,
+										"position",
+										self.outputPath,
+										resolution,
+										str(int(args[4]) - 1),
+										args[5],
+										str(args[3]),
+										str(args[-1]).lower())
+
+			if args[0][-1] == True:
+				self.bakeColorIDFromSBSBaker(fbxPath,
+										outputName,
+										"color_id",
+										self.outputPath,
+										resolution,
+										str(int(args[4]) - 1),
+										str(args[3]),
+										str(args[-1]).lower(),
+										args[5])
+
+	def bakeColorIDFromSBSBaker(self, *args):
+		fbxPath = args[0]
+		outputName = args[1]
+		pattern = args[2]
+		outputPath = args[3]
+		resolution = args[4]
+		uvSet = args[5]
+		quality = args[6]
+		lowAsHigh = args[7]
+		outputFormat = args[-1]
+
+
+		subprocess.call([self.SBSBakerLocation, 
+			"--verbose",
+			"color-from-mesh",
+			"--inputs", fbxPath,
+			"--output-name", outputName + "_" + pattern, 
+			"--output-path", outputPath,
+			"--output-size", resolution,
+			"--antialiasing", quality,
+			"--uv-set", uvSet,
+			"--output-format", outputFormat,
+			"--color-source", "1",
+			"--use-lowdef-as-highdef", lowAsHigh])				
+
+	def bakePositionFromSBSBaker(self, *args):
+		fbxPath = args[0]
+		outputName = args[1]
+		pattern = args[2]
+		outputPath = args[3]
+		resolution = args[4]
+		uvSet = args[5]
+		outputFormat = args[6]
+		quality = args[7]
+		lowAsHigh = args[-1]
+
+		subprocess.call([self.SBSBakerLocation, 
+			"--verbose",
+			"position-from-mesh",
+			"--inputs", fbxPath,
+			"--output-name", outputName + "_" + pattern, 
+			"--output-path", outputPath,
+			"--output-size", resolution,
+			"--antialiasing", quality,
+			"--uv-set", uvSet,
+			"--output-format", outputFormat,
+			"--use-lowdef-as-highdef", lowAsHigh,
+			"--dilation-width", "4",
+			"--mode", "0",
+			"--normalization", "1",
+			])
+
+	def bakeWorldSpaceNormalFromSBSBaker(self, *args):
+		fbxPath = args[0]
+		outputName = args[1]
+		pattern = args[2]
+		outputPath = args[3]
+		resolution = args[4]
+		uvSet = args[5]
+		outputFormat = args[6]
+		normalMap = args[-1]
+
+		subprocess.call([self.SBSBakerLocation, 
+			"--verbose",
+			"normal-world-space",
+			"--inputs", fbxPath,
+			"--output-name", outputName + "_" + pattern, 
+			"--output-path", outputPath,
+			"--output-size", resolution,
+			"--uv-set", uvSet,
+			"--output-format", outputFormat,
+			"--dilation-width", "4",
+			"--normal-format", "0",
+			"--normal", normalMap
+			])
+
+	def bakeCurvatureFromSBSBaker(self, *args):
+		fbxPath = args[0]
+		outputName = args[1]
+		pattern = args[2]
+		outputPath = args[3]
+		resolution = args[4]
+		uvSet = args[5]
+		outputFormat = args[-1]
+
+		subprocess.call([self.SBSBakerLocation, 
+			"--verbose",
+			"curvature",
+			"--inputs", fbxPath,
+			"--output-name", outputName + "_" + pattern, 
+			"--output-path", outputPath,
+			"--output-size", resolution,
+			"--uv-set", uvSet,
+			"--output-format", outputFormat,
+			"--dilation-width", "4",
+			"--details", "0.25"
+			])
+
 
 	def bakeAOFromSBSBaker(self, *args):
-		SBSBakerLocation = self.getSBSBaker()
-		if SBSBakerLocation != None:
-			print (args)
-			fbxPath = args[0]
-			outputName = args[1]
-			pattern = args[2]
-			outputPath = args[3]
-			resolution = args[4]
-			quality = args[5]
-			lowAsHigh = args[6]
-			uvSet = args[7]
-			outputFormat = args[8]
-			ignoreBackFace = args[9]
-			normalMap = args[-1]
+		fbxPath = args[0]
+		outputName = args[1]
+		pattern = args[2]
+		outputPath = args[3]
+		resolution = args[4]
+		quality = args[5]
+		lowAsHigh = args[6]
+		uvSet = args[7]
+		outputFormat = args[8]
+		ignoreBackFace = args[9]
+		normalMap = args[-1]
 
 
-			subprocess.call([SBSBakerLocation, 
-				"--verbose",
-				"ambient-occlusion-from-mesh",
-				"--inputs", fbxPath,
-				"--output-name", outputName + "_" + pattern, 
-				"--output-path", outputPath,
-				"--output-size", resolution,
-				"--antialiasing", quality,
-				"--use-lowdef-as-highdef", lowAsHigh,
-				"--uv-set", uvSet,
-				"--output-format", outputFormat,
-				"--dilation-width", "4",
-				"--ignore-backface", ignoreBackFace,
-				"--ignore-backface-secondary", "0",
-				"--nb-second-rays", "64",
-				"--normal", normalMap,
-				"--normal-format", "0"], shell=False)
+		subprocess.call([self.SBSBakerLocation, 
+			"--verbose",
+			"ambient-occlusion-from-mesh",
+			"--inputs", fbxPath,
+			"--output-name", outputName + "_" + pattern, 
+			"--output-path", outputPath,
+			"--output-size", resolution,
+			"--antialiasing", quality,
+			"--use-lowdef-as-highdef", lowAsHigh,
+			"--uv-set", uvSet,
+			"--output-format", outputFormat,
+			"--dilation-width", "4",
+			"--ignore-backface", ignoreBackFace,
+			"--ignore-backface-secondary", "0",
+			"--nb-second-rays", "64",
+			"--normal", normalMap,
+			"--normal-format", "0"], shell=False)
 
 	def getFullOutputPath(self):
 		self.outputPath, fileType = QtWidgets.QFileDialog.getSaveFileName(None, "Save File", "")
